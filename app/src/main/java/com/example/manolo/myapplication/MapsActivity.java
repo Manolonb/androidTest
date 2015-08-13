fbd586d4f4a75aa3438205bdb3acc66e491cc30a
unknown <manolonac@vielca-2011-120.vielcahp.com>
2015-08-13 10:30:33 +0200

    Añadido comentario

--- app/src/main/java/com/example/manolo/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/manolo/myapplication/MapsActivity.java
@@ -8,6 +8,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
+
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
@@ -41,6 +43,8 @@ public class MapsActivity extends FragmentActivity {
      * method in {@link #onResume()} to guarantee that it will be called.
      */
     private void setUpMapIfNeeded() {
+		//Soy un comentario que se mueve entre pcs! :-D
+		if (1=1);
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
